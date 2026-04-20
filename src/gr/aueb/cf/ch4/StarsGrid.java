ffdb5d73cb41464fe1b3e8c5162bbe867a9588a7
AthTop <AthTop@users.noreply.github.com>
2026-04-20 13:44:15 +0300

    chore(ch4): exercise 1.3, printing n x n stars

---
+++ src/gr/aueb/cf/ch4/StarsGrid.java
@@ -0,0 +1,27 @@
+package gr.aueb.cf.ch4;
+
+import java.util.Scanner;
+
+/**
+ * Prints user inputted n stars in a n x n grid
+ */
+public class StarsGrid {
+
+    public static void main(String[] args) {
+        // Initialization
+        Scanner scanner = new Scanner(System.in);
+        int n = 0;
+
+        // User input
+        System.out.println("Input number of stars to print: ");
+        n = scanner.nextInt();
+
+        // Print stars
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+}
